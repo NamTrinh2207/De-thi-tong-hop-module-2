18daa7b00afaf0c6022fe04839124e284a27fb55
unknown <trinhnam180697@gmail.com>
2023-02-05 01:03:05 +0700

    Quản lý tiền điện

--- thuc-hanh-2/src/display/Run.java
+++ thuc-hanh-2/src/display/Run.java
@@ -28,7 +28,7 @@ public class Run {
             Client client = new Client(fullName, address, electricMeterCode);
             return new Receipt(client, oldIndex, newIndex);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            System.err.println(e.getMessage());
         }
         return null;
     }
